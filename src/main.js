93ace6bb6c7eb32fd88b44df4f18776b9b17cdb1
Kamron <meganyuton1@gmail.com>
2018-10-09 10:42:31 +0500

    shelfs correctly positions

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ let furniture = {
     layer:{},
     shelfGroup:{},
     shelfsDefaultPositions : {},
+    limitation: {},
     height: 280,
     width:380,
     createLeftSection: function() {
@@ -72,8 +73,6 @@ let furniture = {
         polyBot.setZIndex(1);
     },
     createShelf: function() {
-
-
         let shelf = new Konva.Line({
             points: [50, 225, 90, 182, 285, 182, 245, 225, 50, 225],
             fill: 'white',
@@ -99,15 +98,28 @@ let furniture = {
                 }
             }
         });
+        // shelf.on('dragend', (e) => {
+        //     let pos = this.stage.getPointerPosition();
+        //     let shape = this.layer.getIntersection(pos);
+        //
+        //     e.target.attrs.points[1] = e.target.attrs.points[1] + e.target.attrs.y;
+        //     e.target.attrs.points[3] = e.target.attrs.points[3] + e.target.attrs.y;
+        //     e.target.attrs.points[5] = e.target.attrs.points[5] + e.target.attrs.y;
+        //     e.target.attrs.points[7] = e.target.attrs.points[7] + e.target.attrs.y;
+        //     e.target.attrs.points[9] = e.target.attrs.points[9] + e.target.attrs.y;
+        //
+        //     this.layer.draw();
+        // });
+
         this.shelfsDefaultPositions['shelfX1'] = shelf.attrs.points[4];
         this.shelfsDefaultPositions['shelfX2'] = shelf.attrs.points[6];
         this.shelfGroup = new Konva.Group({
             x: 0,
-            y: 0
+            y: 0,
+            height: this.height
         });
         this.shelfGroup.add(shelf);
         this.layers[this.layers.length] = this.shelfGroup;
-        //this.layers[this.layers.length] = shelf;
 
         this.shelfGroup.setZIndex(3);
         shelf.setZIndex(3);
@@ -124,64 +136,119 @@ let furniture = {
             strokeWidth: 1,
             closed : true,
             draggable: true,
-            name: 'shelf',
-            dragBoundFunc: (pos) => {
-                let minY = this.limitation.top;
-                let maxY = this.limitation.bottom;
-                let newY;
-                if(pos.y < (maxY-minY)/2*(-1) + 20){
-                    newY = (maxY-minY)/2*(-1) + 20;
-                }else if (pos.y > (maxY-minY)/2 - 20 ){
-                    newY = (maxY-minY)/2 - 20;
-                }else{
-                    newY = pos.y;
-                }
-                return {
-                    x: 0,
-                    y: newY
-                }
-            }
+            name: 'shelf'
         });
         if(!this.shelfsDefaultPositions.hasOwnProperty('shelfX1') && !this.shelfsDefaultPositions.hasOwnProperty('shelfX2')){
             this.shelfsDefaultPositions['shelfX1'] = shelf.attrs.points[4];
             this.shelfsDefaultPositions['shelfX2'] = shelf.attrs.points[6];
         }
 
-        this.layer.add(shelf);
+        this.shelfGroup.add(shelf);
         shelf.setZIndex(3);
+        this.shelfGroupPosition(this.shelfGroup.children);
         this.layer.draw();
+    },
+
+    shelfGroupPosition(children){
+        if(children){
+            let count = children.length,
+                interval = (this.height - 40)/count,
+                i=1,
+                center = [50, 225, 90, 182, 285, 182, 245, 225, 50, 225];
+
+            for(let y = children.length; y >= 0; y--){
+                if(children.hasOwnProperty(y) && children[y].hasOwnProperty('attrs')){
+                    let attrs = children[y]['attrs'];
+                    if(attrs && attrs.hasOwnProperty('name') && attrs.name == 'shelf'){
+                        //attrs.points[0] = 50;
+                        attrs.points[1] = interval*i + 90 - interval/2;
+                        //attrs.points[2] = 90;
+                        attrs.points[3] = interval*i - 43 + 90 - interval/2;
+                        //attrs.points[4] = 285;
+                        attrs.points[5] = interval*i - 43 + 90 - interval/2;
+                        //attrs.points[6] = 245;
+                        attrs.points[7] = interval*i + 90 - interval/2;
+                        //attrs.points[8] = 50;
+                        attrs.points[9] = interval*i + 90 - interval/2;
+                        i++;
+
+                        attrs.dragBoundFunc = (pos) => {
+                            let minY = this.limitation.top;
+                            let maxY = this.limitation.bottom;
+                            let newY;
+                            let isFirst = !children.hasOwnProperty(y+1);
+                            let isLast = !children.hasOwnProperty(y-1);
+                            let newChildren;
 
 
 
-        let count = 0;
-        console.log(this.stage);
-        for(let key in this.stage){
-            if(this.stage.hasOwnProperty(key)){
-                let children = this.stage[key];
-                for(let key in children){
-                    if(children.hasOwnProperty(key)){
-                        let obj = children[key];
-                        for(let key in obj){
-                            if(obj.hasOwnProperty(key)){
-                                let childs = obj[key];
-                                for(let key in childs){
-                                    if(childs && childs.hasOwnProperty(key)){
-                                        let element = childs[key];
-                                        if(element && element.hasOwnProperty('attrs')){
-                                            let attrs = element.attrs;
-                                            if(attrs.hasOwnProperty('name') && attrs.name == 'shelf'){
-                                                count++;
-                                                console.log(childs, count);
-                                            }
+                            for(let key in this.stage.children[0]['children']){
+                                if(this.stage.children[0]['children'].hasOwnProperty(key)){
+                                    let child = this.stage.children[0]['children'][key];
+                                    if(child.hasOwnProperty('nodeType') && child['nodeType'] == 'Group'){
+                                        let shelfGroup = child;
+                                        if(shelfGroup && shelfGroup.hasOwnProperty('children')){
+                                            newChildren = shelfGroup['children'];
                                         }
                                     }
+
                                 }
                             }
-                        }
+
+                            if(isFirst){
+
+                                if(pos.y < (attrs.points[1] - minY - 20)*-1){
+                                    newY = (attrs.points[1] - minY - 20)*-1;
+                                }else if (pos.y > (newChildren[y-1]['attrs']['points'][1] - attrs.points[1] - 20 + (newChildren[y-1]['changedY'] ? newChildren[y-1]['changedY'] : 0))){
+                                    newY = (newChildren[y-1]['attrs']['points'][1] - attrs.points[1] - 20 + (newChildren[y-1]['changedY'] ? newChildren[y-1]['changedY'] : 0));
+                                }else{
+                                    newY = pos.y;
+                                }
+                                return {
+                                    x: 0,
+                                    y: newY
+                                }
+                            }else if (isLast){
+                                if(pos.y < ((attrs.points[1] - newChildren[y+1]['attrs']['points'][1] - 20 - (newChildren[y+1]['changedY'] ? newChildren[y+1]['changedY'] : 0)) *-1)){
+                                    newY = (attrs.points[1] - newChildren[y+1]['attrs']['points'][1] - 20 - (newChildren[y+1]['changedY'] ? newChildren[y+1]['changedY'] : 0))*-1;
+                                    console.log(1);
+                                }else if (pos.y > (maxY-attrs.points[1]) - 20 ){
+                                    newY = (maxY-attrs.points[1]) - 20;
+                                }else{
+                                    newY = pos.y;
+                                }
+                                return {
+                                    x: 0,
+                                    y: newY
+                                }
+                            }else{
+                                if(pos.y < (attrs.points[1] - newChildren[y+1]['attrs']['points'][1] - 20 - (newChildren[y+1]['changedY'] ? newChildren[y+1]['changedY'] : 0))*-1){
+                                    newY = (attrs.points[1] - newChildren[y+1]['attrs']['points'][1] - 20 - (newChildren[y+1]['changedY'] ? newChildren[y+1]['changedY'] : 0))*-1;
+                                }else if (pos.y > (newChildren[y-1]['attrs']['points'][1] - attrs.points[1] - 20 + (newChildren[y-1]['changedY'] ? newChildren[y-1]['changedY'] : 0)) ){
+                                    newY = (newChildren[y-1]['attrs']['points'][1] - attrs.points[1] - 20 + (newChildren[y-1]['changedY'] ? newChildren[y-1]['changedY'] : 0));
+                                }else{
+                                    newY = pos.y;
+                                }
+                                return {
+                                    x: 0,
+                                    y: newY
+                                }
+                            }
+                        };
+                        children[y].on('dragend', (e) => {
+                            let pos = this.stage.getPointerPosition();
+                            let shape = this.layer.getIntersection(pos);
+
+                            children[y]['changedY'] = e.target.attrs.y;
+                            this.layer.draw();
+
+
+                        });
                     }
                 }
             }
         }
+
     },
 
     createSectin: function(){
@@ -207,13 +274,22 @@ let furniture = {
                 for(let key in this.stage.children[0]['children']){
                     if(this.stage.children[0]['children'].hasOwnProperty(key)){
                         let child = this.stage.children[0]['children'][key];
-                        if(child.hasOwnProperty('attrs')){
-                            let attrs = child.attrs;
-                            if(attrs.hasOwnProperty('name') && attrs.name == 'shelf'){
-                                attrs.points[4] = this.shelfsDefaultPositions['currentShelfX1'] = this.shelfsDefaultPositions.shelfX1 + newX;
-                                attrs.points[6] = this.shelfsDefaultPositions['currentShelfX2'] = this.shelfsDefaultPositions.shelfX2 + newX;
+                        if(child.hasOwnProperty('nodeType') && child['nodeType'] == 'Group'){
+                            let shelfGroup = child;
+                            if(shelfGroup && shelfGroup.hasOwnProperty('children')){
+                                let children = shelfGroup['children'];
+                                for(let key in children){
+                                    if(children.hasOwnProperty(key) && children[key].hasOwnProperty('attrs')){
+                                        let attrs = children[key]['attrs'];
+                                        if(attrs && attrs.hasOwnProperty('name') && attrs.name == 'shelf'){
+                                            attrs.points[4] = this.shelfsDefaultPositions['currentShelfX1'] = this.shelfsDefaultPositions.shelfX1 + newX;
+                                            attrs.points[6] = this.shelfsDefaultPositions['currentShelfX2'] = this.shelfsDefaultPositions.shelfX2 + newX;
+                                        }
+                                    }
+                                }
                             }
                         }
+
                     }
                 }
                 return {
@@ -246,7 +322,7 @@ let furniture = {
 
         this.stage.add(layer);
     },
-    limitation: {},
+
     init: function() {
         this.createBotSection();
         this.createLeftSection();
@@ -267,117 +343,3 @@ let addShelf = document.querySelector('.addShelf');
 addShelf.addEventListener('click', function(){
     furniture.addShelf();
 });
-// let width = window.innerWidth;
-// let height = window.innerHeight;
-//
-//
-// let stage = new Konva.Stage({
-//     container: 'app',
-//     width: width,
-//     height: height
-// });
-//
-// let layer = new Konva.Layer();
-//
-// let polyRight = new Konva.Line({
-//     points: [440, 70, 480, 27, 480, 307, 440, 350, 440, 70],
-//     fill: 'white',
-//     stroke: 'black',
-//     strokeWidth: 1,
-//     closed : true
-// });
-//
-// let polyLeft = new Konva.Line({
-//     points: [50, 70, 90, 27, 90, 307, 50, 350, 50, 70],
-//     fill: 'white',
-//     stroke: 'black',
-//     strokeWidth: 1,
-//     closed : true,
-//     name: 'leftSection'
-// });
-//
-//
-//
-// let polyTop = new Konva.Line({
-//     points: [50, 70, 90, 27 , 480, 27, 440, 70, 440, 70],
-//     fill: 'white',
-//     stroke: 'black',
-//     strokeWidth: 1,
-//     closed : true
-// });
-//
-// let polyBot = new Konva.Line({
-//     points: [50, 350, 90, 307 , 480, 307, 440, 350, 440, 350],
-//     fill: 'white',
-//     stroke: 'black',
-//     strokeWidth: 1,
-//     closed : true
-// });
-//
-// let shelf = new Konva.Line({
-//     points: [50, 225, 90, 182, 285, 182, 245, 225, 50, 225],
-//     fill: 'white',
-//     stroke: 'black',
-//     strokeWidth: 1,
-//     closed : true,
-//     draggable: true,
-//     dragBoundFunc: function(pos) {
-//         let minY = polyTop.attrs.points[1];
-//         let maxY = polyBot.attrs.points[1];
-//         console.log(minY,maxY)
-//         let newY;
-//         if(pos.y < (maxY-minY)/2*(-1) + 20){
-//             newY = (maxY-minY)/2*(-1) + 20;
-//         }else if (pos.y > (maxY-minY)/2 - 20 ){
-//             newY = (maxY-minY)/2 - 20;
-//         }else{
-//             newY = pos.y;
-//         }
-//         return {
-//             x: 0,
-//             y: newY
-//         }
-//     }
-// });
-//
-// let shelfsDefaultPositions = {
-//     shelfX1 : shelf.attrs.points[4],
-//     shelfX2 : shelf.attrs.points[6]
-// };
-//
-// let polySection = new Konva.Line({
-//     points: [245, 70, 285, 27, 285, 307, 245, 350, 245, 70],
-//     fill: 'white',
-//     stroke: 'black',
-//     strokeWidth: 1,
-//     closed : true,
-//     draggable: true,
-//     dragBoundFunc: function(pos) {
-//         let minX = polyLeft.attrs.points[0],
-//             maxX = polyRight.attrs.points[0],
-//             newX;
-//         if(pos.x < (maxX-minX)/2*(-1) + 20){
-//             newX = (maxX-minX)/2*(-1) + 20;
-//         }else if (pos.x > (maxX-minX)/2 - 20 ){
-//             newX = (maxX-minX)/2 - 20;
-//         }else{
-//             newX = pos.x;
-//         }
-//         console.log(newX);
-//         shelf.attrs.points[4] = shelfsDefaultPositions.shelfX1 + newX;
-//         shelf.attrs.points[6] = shelfsDefaultPositions.shelfX2 + newX;
-//         console.log(shelf);
-//         return {
-//             x: newX,
-//             y: 0
-//         }
-//     }
-// });
-//
-//
-//
-// // add the shape to the layer
-// layer.add(polyBot,polyLeft,shelf,polySection,polyRight,polyTop);
-//
-// // add the layer to the stage
-// stage.add(layer);
